441fb46c421eb43718bdfedc216639cedc95ca70
coding-beagle <nicholas.teague@focusrite.com>
2025-05-27 20:42:17 +0100

    initial parsing and unit tests done

--- TeagueASM/TeagueASM/parsing/parse.py
+++ TeagueASM/TeagueASM/parsing/parse.py
@@ -1,2 +1,60 @@
+from ..types.instructions import (
+    string_to_instruction,
+    Instruction,
+)  # for initial line validation
+
+
 def parse(file_text: str) -> bool:  # return a success or fail
     return False
+
+
+def initial_parse(file_text: str) -> list[str]:
+    """
+    Initial parse will check if each line either contains
+    a jump tag or an instruction-like string.
+
+    Jump tags will get flattened onto the same line as the next instruction-like string
+    for the second parser to resolve jump addresses.
+
+    Args:
+        file_text (str): input string to be parsed
+
+    Returns:
+        List[str]: A list of instruction like strings
+    """
+
+    flattened_new_lines: str = ""
+
+    for index, line in enumerate(file_text.split("\n")):
+        if line.startswith("#"):
+            flattened_new_lines += "#" + line.split("#")[-1].strip() + "#"
+        else:
+            if not (line.startswith("//")) and (line.strip()):
+                instruction: Instruction = string_to_instruction(
+                    line, index
+                )  # use this for validation, not the cleanest but hey ho
+                if not (instruction.validate_args()):  # type: ignore
+                    raise ValueError(
+                        f"ERROR ON LINE {index}, Call {line} violates arg rules!"
+                    )
+                flattened_new_lines += line + "\n"
+
+    text_string_split: list[str] = flattened_new_lines.split("\n")
+
+    text_string_split_ignore_comments: list[str] = [
+        i.split("//")[0].strip() for i in text_string_split if i != ""
+    ]
+
+    return text_string_split_ignore_comments
+
+
+def second_parser(file_text: str) -> list[str]:
+    """
+    Resolve any jump locations and inline macros.
+
+    Args:
+        file_text (str): _description_
+
+    Returns:
+        list[str]: _description_
+    """
---
+++ TeagueASM/tests/test_parser/test_initial_parser.py
@@ -0,0 +1,55 @@
+import pytest
+from TeagueASM.parsing import initial_parse
+
+testdata = [
+    (
+        """CP 00 00
+# A tag
+IMM 1023 
+     """,
+        ["CP 00 00", "#A tag#IMM 1023"],
+        True,
+        "",
+    ),
+    (
+        """// this is a line comment that should be ignored
+CP 00 12 // some comment
+IMM 1253
+// Another comment""",
+        ["CP 00 12", "IMM 1253"],
+        True,
+        "",
+    ),
+    (
+        """CP asdasdas gdsgjdskgdjs
+IMM 123""",
+        [],
+        False,
+        "line 0",  # check that exception raised contains this
+    ),
+    (
+        """// CP asdasdas gdsgjdskgdjs
+IMM 12412512521512
+IMM 123""",
+        [],
+        False,
+        "line 1",  # check that exception raised contains this
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    ("test_code", "expected", "should_pass", "exception_contains"), testdata
+)
+def test_parsing(
+    test_code: str,
+    expected: bool,
+    should_pass: bool,
+    exception_contains: str,
+):
+    if should_pass:
+        assert initial_parse(test_code) == expected
+    else:
+        with pytest.raises(Exception) as exc_info:
+            initial_parse(test_code)
+        assert exception_contains in str(exc_info.value).lower()  # type: ignore
